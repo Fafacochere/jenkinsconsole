678cf79b714184eae90ff66d7278949ef77b8ffb
Fabien Raulline <f.raulline@aurafinance.fr>
2020-05-26 10:03:25 +0200

    first init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace jenkinsconsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Jenkins Docker !");
+        }
+    }
+}
